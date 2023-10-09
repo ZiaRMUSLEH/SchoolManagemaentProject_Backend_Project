6258d9b0a549a60047f574f29bf3056d2ee0096c
Ziarahman Musleh <ziarahmanmusleh@gmail.com>
2023-10-09 07:43:47 +0300

    final

--- src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
+++ src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
@@ -36,7 +36,6 @@ public class SchoolManagementApplication implements CommandLineRunner {
             userRoleService.saveUserRole(RoleType.TEACHER);
             userRoleService.saveUserRole(RoleType.STUDENT);
             userRoleService.saveUserRole(RoleType.ADVISORY_TEACHER);
-
         }
 
         if(adminService.countAllAdmins()==0){
